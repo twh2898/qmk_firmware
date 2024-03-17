4c7c6ee860ff130d23b8fea8c52541b0f2ab64e2
Thomas Harrison <twh2898@vt.edu>
2024-03-16 22:37:25 -0400

    Working layer colors

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/twh2898/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/twh2898/keymap.c
@@ -101,45 +101,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // clang-format on
 
-// #define MATRIX_ROWS 4
-// #define MATRIX_COLS 12
-//
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
 // #ifdef RGB_MATRIX_ENABLED
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//     RGB ledcol[NUM_LAYERS] = { {RGB_BLUE}, {RGB_CYAN}, {RGB_RED}, {RGB_PURPLE}, {RGB_GREEN} };
-//     uint8_t layer = get_highest_layer(layer_state);
-//
-//   // For special layers: Set the key LEDs, overwriting effects
-//   if (layer > _QWERTY) {
-//     for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-//       for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-//         uint8_t index = g_led_config.matrix_co[row][col];
-//
-//         // Valid LED on that position?
-//         if ((index >= led_min) && (index < led_max) && (index != NO_LED)) {
-//           // Highlight keys with special functions
-//           if (keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-//             rgb_matrix_set_color(index, ledcol[layer].g, ledcol[layer].r, ledcol[layer].b);
-//           }
-//         }
-//       }
-//     }
-//   }
-//
-//   // All layers: Underglow and Indicators (with reduced intensity)
-//   for (uint8_t i = led_min; i < led_max; i++) {
-//     if (g_led_config.flags[i] == LED_FLAG_UNDERGLOW) {
-//       rgb_matrix_set_color(i, ledcol[layer].g, ledcol[layer].r, ledcol[layer].b);
-//     }
-//     if (g_led_config.flags[i] == LED_FLAG_INDICATOR) {
-//       rgb_matrix_set_color(i, ledcol[layer].g>>4, ledcol[layer].r>>4, ledcol[layer].b>>4);
-//     }
-//   }
-//
-//   return false;
-// }
-//
-// #endif // RGB_MATRIX_ENABLED
+#ifndef RGB_MATRIX_ENABLED
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    RGB ledcol[NUM_LAYERS] = { {RGB_BLUE}, {RGB_CYAN}, {RGB_GREEN}, {RGB_PURPLE} };
+    uint8_t layer = get_highest_layer(layer_state);
+
+  // For special layers: Set the key LEDs, overwriting effects
+  if (layer > _QWERTY) {
+    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+      for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+        uint8_t index = g_led_config.matrix_co[row][col];
+
+        // Valid LED on that position?
+        if ((index >= led_min) && (index < led_max) && (index != NO_LED)) {
+          // Highlight keys with special functions
+          if (keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+            rgb_matrix_set_color(index, ledcol[layer].g, ledcol[layer].r, ledcol[layer].b);
+          }
+        }
+      }
+    }
+  }
+
+  // All layers: Underglow and Indicators (with reduced intensity)
+  for (uint8_t i = led_min; i < led_max; i++) {
+    if (g_led_config.flags[i] == LED_FLAG_UNDERGLOW) {
+      rgb_matrix_set_color(i, ledcol[layer].g, ledcol[layer].r, ledcol[layer].b);
+    }
+    if (g_led_config.flags[i] == LED_FLAG_INDICATOR) {
+      rgb_matrix_set_color(i, ledcol[layer].g>>4, ledcol[layer].r>>4, ledcol[layer].b>>4);
+    }
+  }
+
+  return false;
+}
+
+#endif // RGB_MATRIX_ENABLED
 
 #define RBG_VAL 100
 
@@ -163,6 +164,12 @@ void bg_layer(enum layers layer) {
 
 void keyboard_post_init_user(void) {
     bg_layer(_QWERTY);
+    for (int i = 0; i < 10; i++) {
+        rgb_matrix_set_color(i, 0, 0, 0);
+    }
+    for (int i = 0; i < 10; i++) {
+        rgb_matrix_set_color(i, 0, 0, 0);
+    }
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -171,7 +178,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         default:
         case _QWERTY:
             // Default colors
-            // rgb_matrix_mode(RGB_MATRIX_HUE_WAVE);
+            rgb_matrix_mode(RGB_MATRIX_HUE_WAVE);
             break;
         case _LOWER:
             // green-ish
